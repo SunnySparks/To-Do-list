bf3ddad7eca8f49c9a5cce39feecf84d7833c8bc
SunnySparks <Josediazt6@outlook.com>
2021-09-22 13:38:47 -0500

    Add function to create tasks and display in the HTML document

--- src/index.js
+++ src/index.js
@@ -1,21 +1,81 @@
 import _ from 'lodash';
 import './style.css';
-//import Icon from './icon.png';
+import 'bootstrap';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    //const myIcon = new Image();
-    //myIcon.src = Icon;
-
-    //element.appendChild(myIcon);
-  
-    return element;
+var todoTasks = [
+  {
+    "description": "Do the dishes / Sample element",
+    "completed": false,
+    "index": 0,
+  },
+  {
+    "description": "Do the laundry / Sample element",
+    "completed": false,
+    "index": 1,
+  },
+  {
+    "description": "Walk the dog / Sample element",
+    "completed": false,
+    "index": 2,
   }
+];
+
+//elements
+var input = searchID("todoInput");
+
+//Selectors
+const todoButton = document.querySelector(".todo-button");
+const todoInput = document.querySelector(".todoInput");
+
+//Event Listeners
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+      fillOutList(todoTasks);
+  }
+});
+
+//Functions
+function searchID(id) {
+  return document.getElementById(id);
+}
+
+function fillOutList(list) {
+  const bookObjects = list;
+
+  bookObjects.forEach((element) => {
+    const taskIndex = bookObjects.indexOf(element);
+    return taskIndex;
+  });
+
+  const listWrapper = searchID("listWrapper");
+
+  const listElement = document.createElement('li');
+  listElement.classList.add('list-group-item', 'pl-5', 'pt-4', 'pb-4', 'clearfix');
+
+  const checkBox = document.createElement('input');
+  checkBox.type = "checkbox";
+  checkBox.classList.add('form-check-input', 'float-left');
+
+  const listText = document.createElement('span');
+  listText.innerText = todoInput.value;
+
+  const dragIcon = document.createElement('span');
+  dragIcon.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
+  dragIcon.classList.add('float-right');
+
+  listElement.appendChild(checkBox);
+  listElement.appendChild(listText);
+  listElement.appendChild(dragIcon);
+  listWrapper.appendChild(listElement);
+
+  const pushing = {description : todoInput.value, completed: false, index : bookObjects.length}
+  bookObjects.push(pushing);
+
+  todoTasks = bookObjects;
   
-  document.body.appendChild(component());
+
+  todoInput.value = "";
+}
+
+
+
