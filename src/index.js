b2f79771b869acfddb34378ac6aa4e89d7d82d16
SunnySparks <Josediazt6@outlook.com>
2021-09-23 20:44:57 -0500

    Add Status Toggler to main function
    Add saving to localstorage function.
    Fix bug that refilled task text with empty text.
    Remove fillers module as it was the main cause of the bug

--- src/index.js
+++ src/index.js
@@ -1,79 +1,68 @@
 import './style.css';
 import 'bootstrap';
-import { createTask } from './fillers.js';
-import { statusUpdater } from './updates.js';
-import { statusToggler }  from './updates.js';;
+import { statusToggler } from './updates.js';
 export { fillOutList };
 
-
-
 class Task {
-  constructor(description, index) {
+  constructor(description, index, completed = false) {
     this.description = description;
     this.index = index;
-    this.completed = false;
+    this.completed = completed;
   }
 }
 
-
-const todoTasks = [
-  
-];
-
-// Selectors
+var todoTasks = [
+  new Task("Do the dishes", false, 0,),
+  new Task("Do the laundry", false, 1),
+  new Task("Walk the dog", false, 2)
+]
 
 // Functions
 let searchID = (id) => {
   return document.getElementById(id);
 }
 
-const todoInput = document.querySelector('.todoInput');
-var input = searchID("todoInput");
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-      filler(todoTasks);
-  }
-});
 
-let filler = (list) => {
+function fillOutList(list){
+  console.log(list, typeof list);
   const listWrapper = searchID('listWrapper');
-  const listElement = document.createElement('li');
-    listElement.classList.add('list-group-item', 'pl-5', 'pt-4', 'pb-4', 'clearfix');
-
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    checkBox.classList.add('form-check-input', 'float-left');
-    listElement.appendChild(checkBox);
-
-    const listText = document.createElement('span');
-    listText.innerText = todoInput.value;
-    listElement.appendChild(listText);
-
-    const dragIcon = document.createElement('span');
-    dragIcon.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
-    dragIcon.classList.add('float-right');
-    listElement.appendChild(dragIcon);
-
-    listWrapper.appendChild(listElement);
-
-    console.log('it finally entered');
-    const pushing = { description: todoInput.value, completed: false, index: list.length };
-    todoTasks.push(pushing);
-   
-    todoInput.value = '';
-    console.log('list', list);
-}
-
-let fillOutList = (list) => {
+  list.sort((a, b) => a.index - b.index);
   list.forEach((task) => {
-    createTask(task);
-    console.log('task', task);
-    console.log('description', task.description);
-    filler(list);
+    const listElement = document.createElement('li');
+      listElement.classList.add('list-group-item', 'pl-5', 'pt-4', 'pb-4', 'clearfix');
+  
+      const checkBox = document.createElement('input');
+      checkBox.type = 'checkbox';
+      checkBox.classList.add('form-check-input', 'float-left');
+      listElement.appendChild(checkBox);
+      statusToggler(checkBox, list);
+      
+  
+      const listText = document.createElement('span');
+      listText.innerText = task.description;
+      localStorage.setItem
+      listElement.appendChild(listText);
+  
+      const dragIcon = document.createElement('span');
+      dragIcon.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
+      dragIcon.classList.add('float-right');
+      listElement.appendChild(dragIcon);
+  
+      listWrapper.appendChild(listElement);
+  
+      if (todoInput.value.length > 0) {
+      const pushing = { description: todoInput.value, completed: false, index: list.length };
+      todoTasks.push(pushing);
+      localStorage.setItem('pushing', JSON.stringify(todoTasks));
+      }
   });
 }
 
+
 window.onload = () => {
+  const retrieve = JSON.parse(localStorage.getItem('pushing'));
+  if (retrieve !== null) {
+    todoTasks = retrieve;
+  }
   fillOutList(todoTasks);
-  statusUpdater(todoTasks);
 };
--- src/updates.js
+++ src/updates.js
@@ -1,17 +1,16 @@
-export { statusToggler };
-export { statusUpdater };
+export { statusToggler};
 
-//Selectors
-const taskToggler = document.querySelector('.form-check-input')
 
 //Functions
-let statusToggler = (button) => {
-    button.addEventListener("click", statusUpdater);
+let statusToggler = (box, list) => {
+    box.addEventListener('change', function(e){
+        let nextSibling = box.nextElementSibling.innerHTML;
+        for (let i = 0; i < list.length; i++) {
+            if (box.checked && nextSibling === list[i].description){
+                list[i].completed = true;
+            }else if (box.unchecked&& nextSibling === list[i].description){
+                list[i].completed = false;
+            }
+        }
+      });
 }
-
-let statusUpdater = (list) => {
-    list.forEach((task) => {
-        console.log(task);
-    })
-}
-
