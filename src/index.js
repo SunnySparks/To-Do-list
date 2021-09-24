e065694f7da915c83aea41acefdc58164dcd22e1
SunnySparks <Josediazt6@outlook.com>
2021-09-23 20:57:11 -0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css';
 import 'bootstrap';
-import { statusToggler } from './updates.js';
-export { fillOutList };
+import statusToggler from './updates.js';
 
 class Task {
   constructor(description, index, completed = false) {
@@ -11,53 +10,44 @@ class Task {
   }
 }
 
-var todoTasks = [
-  new Task("Do the dishes", false, 0,),
-  new Task("Do the laundry", false, 1),
-  new Task("Walk the dog", false, 2)
-]
+let todoTasks = [
+  new Task('Do the dishes', false, 0),
+  new Task('Do the laundry', false, 1),
+  new Task('Walk the dog', false, 2),
+];
 
 // Functions
-let searchID = (id) => {
-  return document.getElementById(id);
-}
+const searchID = (id) => document.getElementById(id);
 
-
-function fillOutList(list){
+function fillOutList(list) {
   console.log(list, typeof list);
   const listWrapper = searchID('listWrapper');
-  list.sort((a, b) => a.index - b.index);
   list.forEach((task) => {
     const listElement = document.createElement('li');
-      listElement.classList.add('list-group-item', 'pl-5', 'pt-4', 'pb-4', 'clearfix');
-  
-      const checkBox = document.createElement('input');
-      checkBox.type = 'checkbox';
-      checkBox.classList.add('form-check-input', 'float-left');
-      listElement.appendChild(checkBox);
-      statusToggler(checkBox, list);
-      
-  
-      const listText = document.createElement('span');
-      listText.innerText = task.description;
-      localStorage.setItem
-      listElement.appendChild(listText);
-  
-      const dragIcon = document.createElement('span');
-      dragIcon.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
-      dragIcon.classList.add('float-right');
-      listElement.appendChild(dragIcon);
-  
-      listWrapper.appendChild(listElement);
-  
-      if (todoInput.value.length > 0) {
-      const pushing = { description: todoInput.value, completed: false, index: list.length };
-      todoTasks.push(pushing);
-      localStorage.setItem('pushing', JSON.stringify(todoTasks));
-      }
+    listElement.classList.add('list-group-item', 'pl-5', 'pt-4', 'pb-4', 'clearfix');
+
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.classList.add('form-check-input', 'float-left');
+    listElement.appendChild(checkBox);
+    statusToggler(checkBox, list);
+
+    const listText = document.createElement('span');
+    listText.innerText = task.description;
+    listElement.appendChild(listText);
+
+    const dragIcon = document.createElement('span');
+    dragIcon.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
+    dragIcon.classList.add('float-right');
+    listElement.appendChild(dragIcon);
+
+    listWrapper.appendChild(listElement);
+
+    const pushing = { description: task.description, completed: false, index: task.index };
+    todoTasks.push(pushing);
+    localStorage.setItem('pushing', JSON.stringify(todoTasks));
   });
 }
-
 
 window.onload = () => {
   const retrieve = JSON.parse(localStorage.getItem('pushing'));
--- src/updates.js
+++ src/updates.js
@@ -1,16 +1,14 @@
-export { statusToggler};
-
-
-//Functions
-let statusToggler = (box, list) => {
-    box.addEventListener('change', function(e){
-        let nextSibling = box.nextElementSibling.innerHTML;
-        for (let i = 0; i < list.length; i++) {
-            if (box.checked && nextSibling === list[i].description){
-                list[i].completed = true;
-            }else if (box.unchecked&& nextSibling === list[i].description){
-                list[i].completed = false;
-            }
-        }
-      });
+function statusToggler(box, list) {
+  box.addEventListener('change', () => {
+    const nextSibling = box.nextElementSibling.innerHTML;
+    for (let i = 0; i < list.length; i += 1) {
+      if (box.checked && nextSibling === list[i].description) {
+        list[i].completed = true;
+      } else if (box.unchecked && nextSibling === list[i].description) {
+        list[i].completed = false;
+      }
+    }
+  });
 }
+
+export default statusToggler;
