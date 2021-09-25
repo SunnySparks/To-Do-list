c2ecf5c4adb5259d2c60c98c7ff6b1d6b448463c
SunnySparks <Josediazt6@outlook.com>
2021-09-25 00:41:07 -0500

    Fix linter errors

--- src/add_remove.js
+++ src/add_remove.js
@@ -1,61 +1,61 @@
-import { fillOutList } from './index.js';
-export { createTask, taskEdit, clicky, deleteCompleted };
+/* eslint-disable import/no-cycle */
+import fillOutList from './index.js';
 
-
-
-function createTask(list, input){
-    class Task {
-        constructor(description, completed, index) {
-          this.description = description;
-          this.completed = false;
-          this.index = index;
-        }
+function createTask(list, input) {
+  class Task {
+    constructor(description, completed, index) {
+      this.description = description;
+      this.completed = false;
+      this.index = index;
     }
-    const new1 = new Task(input, false);
-    return new1
+  }
+  const new1 = new Task(input, false);
+  return new1;
 }
 
 function taskEdit(task, list, element) {
-    task.addEventListener("input", function(event) {
-        element.description = task.value;
-    });
-        task.addEventListener("keyup", function(event) {
-        if (event.keyCode === 13) {
-            console.log("enter");
-            localStorage.setItem('pushing', JSON.stringify(list));
-          }
-    });
+  task.addEventListener('input', () => {
+    element.description = task.value;
+  });
+  task.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+      console.log('enter');
+      localStorage.setItem('pushing', JSON.stringify(list));
+    }
+  });
+}
 
+function filter(list) {
+  localStorage.setItem('pushing', JSON.stringify(list));
+  const container = document.getElementById('listWrapper');
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  const obtain = JSON.parse(localStorage.getItem('pushing'));
+  fillOutList(obtain);
 }
 
 function clicky(element, list, index) {
-    element.addEventListener("click", function(event){
-        list = list.filter((el) => el.index != index.index);
-        filter(list);
-
-    });
+  element.addEventListener('click', () => {
+    list = list.filter((el) => el.index !== index.index);
+    filter(list);
+  });
 }
 
-function deleteCompleted(button, list, index){
-    button.addEventListener("click", function(event){
-        //console.log("deleteAll");
-        for (let i = 0; i < list.length; i += 1) {
-            console.log("completed", list[i].completed);
-        }
-        console.log(index.completed);
-            console.log('before', list);
-            list = list.filter((el) => el.completed != true);
-
-            filter(list);
-    });
-}
-
-function filter(list){
-    localStorage.setItem('pushing', JSON.stringify(list));
-    var container = document.getElementById("listWrapper");
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
+function deleteCompleted(button, list, index) {
+  button.addEventListener('click', () => {
+    // console.log("deleteAll");
+    for (let i = 0; i < list.length; i += 1) {
+      console.log('completed', list[i].completed);
     }
-    const obtain = JSON.parse(localStorage.getItem('pushing'));
-    fillOutList(obtain);
+    console.log(index.completed);
+    console.log('before', list);
+    list = list.filter((el) => el.completed !== true);
+
+    filter(list);
+  });
 }
+
+export {
+  createTask, taskEdit, clicky, deleteCompleted,
+};
--- src/index.js
+++ src/index.js
@@ -1,79 +1,71 @@
+/* eslint-disable import/no-cycle */
 import './style.css';
 import 'bootstrap';
 import statusToggler from './updates.js';
-import { createTask, taskEdit, clicky, deleteCompleted } from './add_remove.js';
-import { indexOf } from 'lodash';
-export { fillOutList };
+import {
+  createTask, taskEdit, clicky, deleteCompleted,
+} from './add_remove.js';
 
-
-export let todoTasks = [
+let todoTasks = [
 
 ];
 
-
 // Functions
 const searchID = (id) => document.getElementById(id);
-var deleteButton = searchID("delete-all");
-
-
+const deleteButton = searchID('delete-all');
 
 const todoInput = document.querySelector('.todoInput');
-var input = searchID("todoInput");
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13 && todoInput.value != '') {
+const input = searchID('todoInput');
+
+function filler(list) {
+  const listWrapper = searchID('listWrapper');
+  const taskFiller = createTask(list, todoInput.value);
+
+  const listElement = document.createElement('li');
+  listElement.classList.add('list-group-item', 'pl-5', 'pt-4', 'pb-4', 'clearfix');
+
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.classList.add('form-check-input', 'float-left');
+  listElement.appendChild(checkBox);
+  statusToggler(checkBox, list);
+
+  const listText = document.createElement('input');
+  listText.setAttribute('type', 'text');
+  listText.value = taskFiller.description;
+  listElement.appendChild(listText);
+
+  const trashcan = document.createElement('span');
+  trashcan.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  trashcan.classList.add('trashcan', 'float-right', 'pl-3', 'pr-3', 'text-danger');
+  listElement.appendChild(trashcan);
+
+  const dragIcon = document.createElement('span');
+  dragIcon.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
+  dragIcon.classList.add('float-right', 'pl-3', 'pr-3');
+  listElement.appendChild(dragIcon);
+
+  list.forEach((element) => {
+    taskEdit(listText, list, element);
+    clicky(trashcan, list, element);
+    deleteCompleted(deleteButton, list, element);
+  });
+
+  listWrapper.appendChild(listElement);
+  const pushing = { description: taskFiller.description, completed: false, index: list.length };
+  todoTasks.push(pushing);
+  localStorage.setItem('pushing', JSON.stringify(todoTasks));
+}
+
+input.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13 && todoInput.value !== '') {
     filler(todoTasks);
-    input.value = "";
+    input.value = '';
   }
 });
 
-function filler(list){
-  const listWrapper = searchID('listWrapper');
-  let taskFiller = createTask(list, todoInput.value);
-  //console.log(taskFiller, typeof taskFiller);
-    
-    const listElement = document.createElement('li');
-    listElement.classList.add('list-group-item', 'pl-5', 'pt-4', 'pb-4', 'clearfix');
-
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    checkBox.classList.add('form-check-input', 'float-left');
-    listElement.appendChild(checkBox);
-    statusToggler(checkBox, list);
-
-    const listText = document.createElement('input');
-    listText.setAttribute('type', 'text');
-    listText.value = taskFiller.description;
-    //listText.contentEditable;
-    //console.log(taskFiller.description);
-    listElement.appendChild(listText);
-
-    const trashcan = document.createElement('span');
-    trashcan.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    trashcan.classList.add('trashcan', 'float-right', 'pl-3', 'pr-3', 'text-danger');
-    listElement.appendChild(trashcan);
-
-
-    const dragIcon = document.createElement('span');
-    dragIcon.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
-    dragIcon.classList.add('float-right', 'pl-3', 'pr-3');
-    listElement.appendChild(dragIcon);
-
-    list.forEach(element => {
-      taskEdit(listText, list, element);
-      clicky(trashcan, list, element);
-      deleteCompleted(deleteButton, list, element);
-    });
-
-    listWrapper.appendChild(listElement);
-    const pushing = { description: taskFiller.description, completed: false, index: list.length };
-    todoTasks.push(pushing);
-    //console.log(todoTasks);
-    localStorage.setItem('pushing', JSON.stringify(todoTasks));
-}
-
 function fillOutList(list) {
   const listWrapper = searchID('listWrapper');
-  let taskFiller = createTask(list, todoInput.value);
   for (let i = 0; i < list.length; i += 1) {
     const listElement = document.createElement('li');
     listElement.classList.add('list-group-item', 'pl-5', 'pt-4', 'pb-4', 'clearfix');
@@ -81,7 +73,7 @@ function fillOutList(list) {
     const checkBox = document.createElement('input');
     checkBox.type = 'checkbox';
     checkBox.classList.add('form-check-input', 'float-left');
-    if(list[i].completed == true) {
+    if (list[i].completed === true) {
       checkBox.checked = true;
     }
     listElement.appendChild(checkBox);
@@ -90,12 +82,9 @@ function fillOutList(list) {
     const listText = document.createElement('input');
     listText.setAttribute('type', 'text');
     listText.value = list[i].description;
-    //console.log('value', listText.value);
-    //listText.contentEditable;
-    //console.log(taskFiller.description);
     listElement.appendChild(listText);
     taskEdit(listText, list, list[i]);
-    //console.log('text now in main document', list[i].description)
+
     const trashcan = document.createElement('span');
     trashcan.innerHTML = '<i class="fas fa-trash-alt"></i>';
     trashcan.classList.add('trashcan', 'float-right', 'pl-3', 'pr-3', 'text-danger');
@@ -109,15 +98,9 @@ function fillOutList(list) {
 
     listWrapper.appendChild(listElement);
 
-    var description = listText.innerText;
-    var currentIndex = i;
     deleteCompleted(deleteButton, list, list[i]);
   }
 }
-
-
-
-
 
 window.onload = () => {
   const retrieve = JSON.parse(localStorage.getItem('pushing'));
@@ -125,8 +108,8 @@ window.onload = () => {
     todoTasks = retrieve;
   }
   if (todoTasks.length > 0) {
-  fillOutList(todoTasks);
-  } else {
-    console.log(todoTasks.length);
+    fillOutList(todoTasks);
   }
 };
+
+export default fillOutList;
