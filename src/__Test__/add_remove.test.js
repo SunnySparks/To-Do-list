322afc568796dd1e4a0bfc6914f798e1b8d9bbc6
SunnySparks <Josediazt6@outlook.com>
2021-09-30 11:59:19 -0500

    Create initial test

--- src/__Test__/add_remove.test.js
+++ src/__Test__/add_remove.test.js
@@ -1,1 +1,12 @@
-const filter = require('./src/add_remove');
+const createTask = require('./add_remove');
+
+describe('It creates a new object to be stored inside the localStorage'), () => {
+    let list = [];
+    let input = document.getElementById('todoInput');
+    
+    test('it creates a set of HTML elements to be inserted inside the localStorage'), () => {
+        expect(createTask(list, 'hello').description.toBe(
+            Task.description = 'hello'
+        ))
+    }
+}
